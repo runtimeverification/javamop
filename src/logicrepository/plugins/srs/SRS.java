4c335bfb95654d227e70dfb360cdf7cc1471f6da
pmeredit <pmeredit@3664526f-1af9-0fc8-7c89-f5c43c22a1f7>
2012-01-16 09:40:58 +0000

    SRS class
    git-svn-id: https://javamop.googlecode.com/svn/trunk@48 3664526f-1af9-0fc8-7c89-f5c43c22a1f7

---
+++ src/logicrepository/plugins/srs/SRS.java
@@ -0,0 +1,15 @@
+package logicrepository.plugins.srs;
+
+import java.util.ArrayList;
+
+public class SRS extends ArrayList<Rule> {
+  public String toString(){
+    StringBuilder sb = new StringBuilder();
+    for(Rule r : this){
+      sb.append(r.toString());
+      sb.append(",\n");
+    }
+    return sb.toString();
+  }
+}
+
