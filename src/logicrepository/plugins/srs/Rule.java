4ea0d972446b3a9b7900b68b07d9d5da6a310aea
pmeredit <pmeredit@3664526f-1af9-0fc8-7c89-f5c43c22a1f7>
2012-01-23 07:43:46 +0000

    updates to srs
    git-svn-id: https://javamop.googlecode.com/svn/trunk@56 3664526f-1af9-0fc8-7c89-f5c43c22a1f7

--- src/logicrepository/plugins/srs/Rule.java
+++ src/logicrepository/plugins/srs/Rule.java
@@ -8,21 +8,64 @@ public class Rule {
     return lhs.toString() + " -> " + rhs.toString();
   }
 
-
-  //some simplifications to an SRS that make generating pattern match
-  //automata easier.
+  // Simplifying a rule entails first simplifying the sequence on
+  // both sides, all this does right now is remove adjacent Variables
+  // in favor of one Variable, which makes the pattern match automaton
+  // generation faster and less redundant.
   //
-  //1) variables concatenation is idempotent 
-  //   collapse adjacent variables
+  // After the left and right hand side are simplified, we make all rules
+  // total.  This means that they reference the beginning and end of the 
+  // string (^ and $).  If the rule is already total, nothing need be done.
+  // Otherwise we add "^ X*1"  and "X*2 $" to lhs of the rule and X1 and X2 to the
+  // beginning and end of the rhs of the rule, respectively.  If the left hand
+  // side already begins/ends with variables we simply add "^"/"$" respectively.
   //
-  //2) ^A can be removed where A is a Variable
-  //
-  //3) A$ can be removed where A is, again, a variable
-  //
-  //See Sequence.simplify for implementation
+  // Note that X*1 and X*2 will never clash because they are not valid Variables
+  // from the parser
   public void simplify(){
-    lhs = lhs.simplify();
-    rhs = rhs.simplify();
-  }
+   // lhs = lhs.simplify();
+   // rhs = rhs.simplify();
 
+    Symbol begin = Begin.get();
+    Symbol end   = End.get();
+    Symbol X1    = Variable.get("X*1");
+    Symbol X2    = Variable.get("X*2");
+
+    if(lhs.get(0) != begin){
+      if(lhs.get(0) instanceof Variable){
+        Sequence prependBeginTo = new Sequence(lhs.size() + 1);
+        prependBeginTo.add(begin);
+        for(Symbol s : lhs){
+          prependBeginTo.add(s);
+        }
+        lhs = prependBeginTo;
+      }
+      else {
+        Sequence prependBeginVarTo = new Sequence(lhs.size() + 2);
+        prependBeginVarTo.add(begin);
+        prependBeginVarTo.add(X1);
+        for(Symbol s : lhs){
+          prependBeginVarTo.add(s);
+        }
+        lhs = prependBeginVarTo;
+        Sequence prependVarTo = new Sequence(rhs.size() + 1);
+        prependVarTo.add(X1);
+        for(Symbol s : rhs){
+          prependVarTo.add(s);
+        }
+        rhs = prependVarTo;
+      }
+    }
+
+    if(lhs.get(lhs.size() - 1) != end){
+      if(lhs.get(lhs.size() - 1) instanceof Variable){
+        lhs.add(end);      
+      }
+      else {
+        lhs.add(X2);
+        lhs.add(end);
+        rhs.add(X2);
+      }
+    }
+  }
 }
