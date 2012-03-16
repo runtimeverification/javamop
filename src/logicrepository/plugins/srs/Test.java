9733a5543edd3f42e7a22d42048c88d045634893
pmeredit@gmail.com <pmeredit@gmail.com@3664526f-1af9-0fc8-7c89-f5c43c22a1f7>
2012-03-16 15:47:39 +0000

    impl done
    git-svn-id: https://javamop.googlecode.com/svn/trunk@159 3664526f-1af9-0fc8-7c89-f5c43c22a1f7

--- src/logicrepository/plugins/srs/Test.java
+++ src/logicrepository/plugins/srs/Test.java
@@ -1,8 +1,9 @@
 public class Test {
 
-static TransitionImpl [][] pma = {{new TransitionImpl(0, new StateImpl(4, 0)),
-new TransitionImpl(0, new StateImpl(1)),
-},
+static TransitionImpl [][] pma = {
+  {new TransitionImpl(0, new StateImpl(4, 0)),
+  new TransitionImpl(0, new StateImpl(1)),
+  },
 
 {new TransitionImpl(0, new StateImpl(2, new int[] {})),
 new TransitionImpl(0, new StateImpl(3, new int[] {1,})),
@@ -25,6 +26,19 @@ new TransitionImpl(1, new StateImpl(0)),
 
   public static void main(String[] args){
     System.out.println("ran");
+    TransitionImpl t = pma[0][0];
+    System.out.println("s0, next");
+    System.out.println("action = " + t.action);
+    System.out.println("next state = " + t.state.number);
+    System.out.println("next state replacement = " + t.state.replacement);
+    System.out.println("next state category = " + t.state.category);
+
+    System.out.println("s1, hasnext");
+    t = pma[1][1];
+    System.out.println("action = " + t.action);
+    System.out.println("next state = " + t.state.number);
+    System.out.println("next state replacement = " + t.state.replacement);
+    System.out.println("next state category = " + t.state.category);
   }
 }
 
