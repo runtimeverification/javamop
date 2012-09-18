d04c8c9e77eb246f176d383bb07c2ac6ade24a29
pmeredit <pmeredit@3664526f-1af9-0fc8-7c89-f5c43c22a1f7>
2012-09-18 21:42:56 +0000

    fixed MOP logging
    git-svn-id: https://javamop.googlecode.com/svn/trunk@200 3664526f-1af9-0fc8-7c89-f5c43c22a1f7

--- src/javamoprt/MOPLogging.java
+++ src/javamoprt/MOPLogging.java
@@ -35,45 +35,45 @@ public abstract class MOPLogging {
     }
   }
  
-  abstract void println(Level l);
-  abstract void print(Level l, boolean x);
-  abstract void println(Level l, boolean x);
-  abstract void print(Level l, char x);
-  abstract void println(Level l, char x);
-  abstract void print(Level l, char[] x);
-  abstract void println(Level l, char[] x);
-  abstract void print(Level l, double x);
-  abstract void println(Level l, double x);
-  abstract void print(Level l, float x);
-  abstract void println(Level l, float x);
-  abstract void print(Level l, int x);
-  abstract void println(Level l, int x);
-  abstract void print(Level l, long x);
-  abstract void println(Level l, long x);
-  abstract void print(Level l, Object x);
-  abstract void println(Level l, Object x);
-  abstract void print(Level l, String x);
-  abstract void println(Level l, String x);
+  public abstract void println(Level l);
+  public abstract void print(Level l, boolean x);
+  public abstract void println(Level l, boolean x);
+  public abstract void print(Level l, char x);
+  public abstract void println(Level l, char x);
+  public abstract void print(Level l, char[] x);
+  public abstract void println(Level l, char[] x);
+  public abstract void print(Level l, double x);
+  public abstract void println(Level l, double x);
+  public abstract void print(Level l, float x);
+  public abstract void println(Level l, float x);
+  public abstract void print(Level l, int x);
+  public abstract void println(Level l, int x);
+  public abstract void print(Level l, long x);
+  public abstract void println(Level l, long x);
+  public abstract void print(Level l, Object x);
+  public abstract void println(Level l, Object x);
+  public abstract void print(Level l, String x);
+  public abstract void println(Level l, String x);
 
-  void println(){ println(Level.ALL); }
-  void print(boolean x){ println(Level.ALL, x); }
-  void println(boolean x){ println(Level.ALL, x); }
-  void print(char x){ println(Level.ALL, x); }
-  void println(char x){ println(Level.ALL, x); }
-  void print(char[] x){ println(Level.ALL, x); }
-  void println(char[] x){ println(Level.ALL, x); }
-  void print(double x){ println(Level.ALL, x); }
-  void println(double x){ println(Level.ALL, x); }
-  void print(float x){ println(Level.ALL, x); }
-  void println(float x){ println(Level.ALL, x); }
-  void print(int x){ println(Level.ALL, x); }
-  void println(int x){ println(Level.ALL, x); }
-  void print(long x){ println(Level.ALL, x); }
-  void println(long x){ println(Level.ALL, x); }
-  void print(Object x){ println(Level.ALL, x); }
-  void println(Object x){ println(Level.ALL, x); }
-  void print(String x){ print(Level.ALL, x); }
-  void println(String x){ println(Level.ALL, x); }
+  public void println(){ println(Level.ALL); }
+  public void print(boolean x){ println(Level.ALL, x); }
+  public void println(boolean x){ println(Level.ALL, x); }
+  public void print(char x){ println(Level.ALL, x); }
+  public void println(char x){ println(Level.ALL, x); }
+  public void print(char[] x){ println(Level.ALL, x); }
+  public void println(char[] x){ println(Level.ALL, x); }
+  public void print(double x){ println(Level.ALL, x); }
+  public void println(double x){ println(Level.ALL, x); }
+  public void print(float x){ println(Level.ALL, x); }
+  public void println(float x){ println(Level.ALL, x); }
+  public void print(int x){ println(Level.ALL, x); }
+  public void println(int x){ println(Level.ALL, x); }
+  public void print(long x){ println(Level.ALL, x); }
+  public void println(long x){ println(Level.ALL, x); }
+  public void print(Object x){ println(Level.ALL, x); }
+  public void println(Object x){ println(Level.ALL, x); }
+  public void print(String x){ print(Level.ALL, x); }
+  public void println(String x){ println(Level.ALL, x); }
 
   public static void main(String[] args){
     System.out.println("The following levels are enabled by your environment:");
@@ -84,104 +84,6 @@ public abstract class MOPLogging {
 
 }
 
-class MOPLoggingAll extends MOPLogging {
-  PrintStream p;
-  MOPLoggingAll(PrintStream ps) {
-    p = ps;
-  }
-  void println(Level l) { p.println(); }
-  void print(Level l, boolean x){ p.println(x); }
-  void println(Level l, boolean x){ p.println(x); }
-  void print(Level l, char x){ p.println(x); }
-  void println(Level l, char x){ p.println(x); }
-  void print(Level l, char[] x){ p.println(x); }
-  void println(Level l, char[] x){ p.println(x); }
-  void print(Level l, double x){ p.println(x); }
-  void println(Level l, double x){ p.println(x); }
-  void print(Level l, float x){ p.println(x); }
-  void println(Level l, float x){ p.println(x); }
-  void print(Level l, int x){ p.println(x); }
-  void println(Level l, int x){ p.println(x); }
-  void print(Level l, long x){ p.println(x); }
-  void println(Level l, long x){ p.println(x); }
-  void print(Level l, Object x){ p.println(x); }
-  void println(Level l, Object x){ p.println(x); }
-  void print(Level l, String x){ p.println(x); }
-  void println(Level l, String x){ p.println(x); }
-}
 
-class MOPLoggingWarning extends MOPLogging {
-  PrintStream p;
-  MOPLoggingWarning(PrintStream ps) {
-    p = ps;
-  }
-  void println(Level l) { if(l.compareTo(Level.WARNING) >= 0) p.println(); }
-  void print(Level l, boolean x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void println(Level l, boolean x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void print(Level l, char x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void println(Level l, char x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void print(Level l, char[] x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void println(Level l, char[] x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void print(Level l, double x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void println(Level l, double x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void print(Level l, float x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void println(Level l, float x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void print(Level l, int x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void println(Level l, int x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void print(Level l, long x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void println(Level l, long x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void print(Level l, Object x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void println(Level l, Object x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void print(Level l, String x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-  void println(Level l, String x){ if(l.compareTo(Level.WARNING) >= 0) p.println(x); }
-}
 
-class MOPLoggingCritical extends MOPLogging {
-  PrintStream p;
-  MOPLoggingCritical(PrintStream ps) {
-    p = ps;
-  }
-  void println(Level l) { if(l.compareTo(Level.CRITICAL) >= 0) p.println(); }
-  void print(Level l, boolean x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void println(Level l, boolean x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void print(Level l, char x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void println(Level l, char x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void print(Level l, char[] x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void println(Level l, char[] x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void print(Level l, double x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void println(Level l, double x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void print(Level l, float x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void println(Level l, float x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void print(Level l, int x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void println(Level l, int x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void print(Level l, long x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void println(Level l, long x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void print(Level l, Object x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void println(Level l, Object x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void print(Level l, String x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-  void println(Level l, String x){ if(l.compareTo(Level.CRITICAL) >= 0) p.println(x); }
-}
-
-class MOPLoggingNone extends MOPLogging {
-  MOPLoggingNone(PrintStream ps){}
-  void println(Level l) {}
-  void print(Level l, boolean x){}
-  void println(Level l, boolean x){}
-  void print(Level l, char x){}
-  void println(Level l, char x){}
-  void print(Level l, char[] x){}
-  void println(Level l, char[] x){}
-  void print(Level l, double x){}
-  void println(Level l, double x){}
-  void print(Level l, float x){}
-  void println(Level l, float x){}
-  void print(Level l, int x){}
-  void println(Level l, int x){}
-  void print(Level l, long x){}
-  void println(Level l, long x){}
-  void print(Level l, Object x){}
-  void println(Level l, Object x){}
-  void print(Level l, String x){}
-  void println(Level l, String x){}
-}
 
---
+++ src/javamoprt/MOPLoggingNone.java
@@ -0,0 +1,26 @@
+package javamoprt;
+
+import java.io.PrintStream;
+
+public class MOPLoggingNone extends MOPLogging {
+  public MOPLoggingNone(PrintStream ps){}
+  public void println(Level l) {}
+  public void print(Level l, boolean x){}
+  public void println(Level l, boolean x){}
+  public void print(Level l, char x){}
+  public void println(Level l, char x){}
+  public void print(Level l, char[] x){}
+  public void println(Level l, char[] x){}
+  public void print(Level l, double x){}
+  public void println(Level l, double x){}
+  public void print(Level l, float x){}
+  public void println(Level l, float x){}
+  public void print(Level l, int x){}
+  public void println(Level l, int x){}
+  public void print(Level l, long x){}
+  public void println(Level l, long x){}
+  public void print(Level l, Object x){}
+  public void println(Level l, Object x){}
+  public void print(Level l, String x){}
+  public void println(Level l, String x){}
+}
