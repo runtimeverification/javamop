a52e531617faaee96a2b73acbf91e9c720cf28a0
qingzhouluo@gmail.com <qingzhouluo@gmail.com@3664526f-1af9-0fc8-7c89-f5c43c22a1f7>
2012-10-03 15:44:19 +0000

    add missing callback class
    git-svn-id: https://javamop.googlecode.com/svn/trunk@208 3664526f-1af9-0fc8-7c89-f5c43c22a1f7

---
+++ src/javamoprt/MOPCallBack.java
@@ -0,0 +1,16 @@
+package javamoprt;
+
+/**
+ * 
+ * Call back interface for javamoprt interacting with generated aspectj code
+ * 
+ * */
+public interface MOPCallBack {
+
+	/**
+	 * 
+	 * function to be called when passing a callback object
+	 * 
+	 * */
+	public void apply();
+}
