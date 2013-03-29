88b72f8ecb9b1c86bc30babd242fbd2051040739
qingzhouluo@gmail.com <qingzhouluo@gmail.com@3664526f-1af9-0fc8-7c89-f5c43c22a1f7>
2013-03-29 22:50:42 +0000

    add empty file combiner
    git-svn-id: https://javamop.googlecode.com/svn/trunk@259 3664526f-1af9-0fc8-7c89-f5c43c22a1f7

---
+++ src/javamop/util/AJFileCombiner.java
@@ -0,0 +1,18 @@
+package javamop.util;
+
+import java.io.File;
+
+/**
+ * 
+ * Class to combine a Java file with an AspectJ file together.
+ * 
+ * @author Qingzhou Luo
+ * */
+public class AJFileCombiner {
+
+	public static void main(String[] args) {
+		File javaFile = new File(args[0]);
+		File ajFile = new File(args[1]);
+		
+	}
+}
