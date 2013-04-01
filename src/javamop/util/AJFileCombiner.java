9b3a4af5faacc766199722e7d8a8fec8db429098
qingzhouluo@gmail.com <qingzhouluo@gmail.com@3664526f-1af9-0fc8-7c89-f5c43c22a1f7>
2013-04-01 05:49:09 +0000

    implement file combiner, needs to be tested
    git-svn-id: https://javamop.googlecode.com/svn/trunk@261 3664526f-1af9-0fc8-7c89-f5c43c22a1f7

--- src/javamop/util/AJFileCombiner.java
+++ src/javamop/util/AJFileCombiner.java
@@ -1,6 +1,13 @@
 package javamop.util;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 
 /**
  * 
@@ -10,9 +17,75 @@ import java.io.File;
  * */
 public class AJFileCombiner {
 
+	/**
+	 * 
+	 * First parameter: path to java file
+	 * Second parameter: path to aj file
+	 * 
+	 * */
 	public static void main(String[] args) {
+		List<String> javaImports = new ArrayList<String>();
+		List<String> ajImports = new ArrayList<String>();
+		StringBuffer javaCode = new StringBuffer();
+		StringBuffer ajCode = new StringBuffer();
 		File javaFile = new File(args[0]);
 		File ajFile = new File(args[1]);
 		
+		try {
+			Scanner scanner = new Scanner(ajFile);
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				if (line.trim().contains("import")) {
+					ajImports.add(line);
+				}
+				else {
+					ajCode.append(line);
+					ajCode.append("\n");
+				}
+			}
+			
+			scanner = new Scanner(javaFile);
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				if (line.trim().contains("import")) {
+					javaImports.add(line);
+				}
+				else {
+					if (line.contains("public class")) {
+						line = line.replace("public class", "class");
+					}
+					javaCode.append(line);
+					javaCode.append("\n");
+				}
+			}
+			
+			List<String> combinedImports = combineList(javaImports, ajImports);
+			StringBuffer results = new StringBuffer();
+			for (String s : combinedImports) {
+				results.append(s);
+				results.append("\n");
+			}
+			results.append(javaCode);
+			results.append(ajCode);
+			System.out.println(results.toString());
+			FileWriter fw = new FileWriter(ajFile);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(results.toString());
+			bw.flush();
+			bw.close();
+			scanner.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static List<String> combineList(List<String> one, List<String> two) {
+		List<String> result = new ArrayList<String>(one);
+		for (String s : two) {
+			if (!result.contains(s)) {
+				result.add(s);
+			}
+		}
+		return result;
 	}
 }
