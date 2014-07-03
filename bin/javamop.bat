f718806e63e04c27311193523b40acab221dc5f2
A. Cody Schuffelen <astronomix@gmail.com>
2014-07-03 14:37:05 -0500

    Improved line number output

--- bin/javamop.bat
+++ bin/javamop.bat
@@ -2,7 +2,5 @@
 
 set SRC_ROOT=%~dp0..
 
-java -cp "%SRC_ROOT%\lib\rvmonitor.jar;%SRC_ROOT%\lib\rt.jar;%SRC_ROOT%\lib\javamop.jar;%SRC_ROOT%\lib\logicrepository.jar;%SRC_ROOT%\lib\plugins\*.jar;%SRC_ROOT%\lib\mysql-connector-java-3.0.9-stable-bin.jar" javamop.JavaMOPMain %*
-
-
+java -cp "%CLASSPATH%;%SRC_ROOT%\lib\rvmonitor.jar;%SRC_ROOT%\lib\rvmonitorrt.jar;%SRC_ROOT%\lib\javamop.jar;%SRC_ROOT%\lib\logicrepository.jar;%SRC_ROOT%\lib\plugins\*.jar;%SRC_ROOT%\lib\mysql-connector-java-3.0.9-stable-bin.jar" javamop.JavaMOPMain %*
 
