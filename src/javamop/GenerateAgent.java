5d35d365946adf4f5b2a06b16f984b29a51717cf
Philip Daian <philipdaian@rvwork-1.(none)>
2014-09-10 12:29:56 -0500

    readd necessary List import

--- src/javamop/GenerateAgent.java
+++ src/javamop/GenerateAgent.java
@@ -20,6 +20,7 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Handles generating a complete Java agent after .mop files have been processed into .rvm files
