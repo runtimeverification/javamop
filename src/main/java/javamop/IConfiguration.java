3a3084f063a68afdac35c8516e82ec91d83689bb
qingzhou <qingzhouluo@gmail.com>
2014-09-30 21:56:41 -0500

    Extract interface for configuration

---
+++ src/main/java/javamop/IConfiguration.java
@@ -0,0 +1,25 @@
+// Copyright (c) 2002-2014 JavaMOP Team. All Rights Reserved.
+package javamop;
+
+import java.util.Properties;
+
+/**
+ * Interface for all the configurations
+ */
+public interface IConfiguration {
+
+    /**
+     * Get all the property settings for the given configuration
+     *
+     * @return all the property settings
+     */
+    public Properties getAllSettings();
+
+    /**
+     * Get one specific setting from the configuration
+     *
+     * @param key the key of the setting
+     * @return the value of the setting
+     */
+    public String getServerSetting(String key);
+}
---
+++ src/main/java/javamop/TextConfiguration.java
@@ -0,0 +1,67 @@
+// Copyright (c) 2002-2014 JavaMOP Team. All Rights Reserved.
+package javamop;
+
+import javamop.util.Tool;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Class for loading plain text configuration file.
+ */
+public class TextConfiguration implements IConfiguration {
+
+    private final Properties properties;
+
+    public TextConfiguration(String fileName) {
+        final Properties ret = new Properties();
+
+        final String configPath = Tool.getConfigPath();
+        final File configDir = new File(configPath);
+
+        if(!configDir.exists()) {
+            properties = null;
+            return;
+        }
+
+        final File configFile = new File(configPath + File.separator + fileName);
+
+        if(!configFile.exists()) {
+            properties = null;
+            return;
+        }
+
+        try {
+            ret.load(new FileInputStream(configFile.getAbsolutePath()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        properties = ret;
+    }
+
+    /**
+     * Retrieve a property from the server settings file, which is expected to have the name
+     * given in {@code SERVER_SETTING}.
+     * @param key The name of the property to retrieve.
+     * @return The value of the property, or {@code null} if the settings file could not be found.
+     */
+    public String getServerSetting(String key) {
+        if (properties == null) {
+            return null;
+        } else {
+            return properties.getProperty(key);
+        }
+    }
+
+    /**
+     * Get all property settings
+     *
+     * @return all the property settings
+     */
+    public Properties getAllSettings() {
+        return properties;
+    }
+}
