c427bc6da55998901e3002b4607ed9f987a841dc
Qingzhou Luo <qingzhouluo@gmail.com>
2014-10-07 16:06:00 -0500

    Remove un-used and out-dated scripts in JavaMOP/bin

--- src/main/scripts/bin/javamoptestsuite.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set SRC_ROOT=%~dp0..
-
-java -cp "%SRC_ROOT%\lib\rvmonitor.jar;%SRC_ROOT%\lib\rt.jar;%SRC_ROOT%/lib/javamoptestsuite.jar;%SRC_ROOT%/lib/javamop.jar;%SRC_ROOT%/lib/logicrepository.jar:%SRC_ROOT%/lib/*.jar" javamoptestsuite.Main -local "%SRC_ROOT%/examples" %*
-
