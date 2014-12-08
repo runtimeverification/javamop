3aef413e214aaa4ac83eed86b91c8f560f4d6817
Qingzhou Luo <qingzhouluo@gmail.com>
2014-12-08 14:27:18 -0600

    Work in progress

---
+++ src/main/java/javamop/JavaMOPAgentOptions.java
@@ -0,0 +1,33 @@
+// Copyright (c) 2002-2014 JavaMOP Team. All Rights Reserved.
+package javamop;
+
+import com.beust.jcommander.IStringConverter;
+import com.beust.jcommander.Parameter;
+
+import java.io.File;
+
+/**
+ * Command-line options available for controlling JavaMOPAgent
+ */
+public class JavaMOPAgentOptions {
+
+    @Parameter(names={"-n","-agentname"},description = "Use the given agent name instead of " +
+            "agent.jar.")
+    public String agentName;
+
+    @Parameter(names="-excludeJars",description = "By default, rv-monitor-rt.jar and aspectweaver.jar will be" +
+            " included in the generated agent. This option enables users to exclude those jars from the generated" +
+            " agent, so users can resolve the dependency themselves when running the agent.")
+    public boolean excludeJars = false;
+
+    @Parameter(names="-baseaspect",description = "Optionally provide a BaseAspect.aj file " +
+            "for use in generating an agent.", converter = FileConverter.class)
+    public File baseAspect;
+
+    public static class FileConverter implements IStringConverter<File> {
+        @Override
+        public File convert(String value) {
+            return new File(value);
+        }
+    }
+}
