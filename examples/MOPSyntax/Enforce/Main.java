4f7f45be78efcb0bd0646be658ded205c4a7ad19
xiaohe27 <hexiao2@illinois.edu>
2015-05-11 21:15:06 -0500

    add the test files for enforcement test

---
+++ examples/MOPSyntax/Enforce/Main.java
@@ -0,0 +1,17 @@
+class SimpleRunnable implements Runnable{
+    public void run(){
+        System.out.println("Thread " + Thread.currentThread().getName());
+    }
+}
+
+public class Main{
+    public static void main(String args[]) throws Exception{
+        Thread t1 = new Thread (new SimpleRunnable());
+        Thread t2 = new Thread (new SimpleRunnable());
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+
+}
