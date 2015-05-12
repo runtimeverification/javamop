12ab342dd17934db12214b3426e199babc66cb40
xiaohe27 <xiaoguoyi27@gmail.com>
2015-05-12 14:27:28 -0500

    update enforce test case

--- examples/MOPSyntax/Enforce/Main.java
+++ examples/MOPSyntax/Enforce/Main.java
@@ -1,4 +1,10 @@
-class SimpleRunnable implements Runnable{
+class SimpleRunnable1 implements Runnable{
+    public void run(){
+        System.out.println("Thread " + Thread.currentThread().getName());
+    }
+}
+
+class SimpleRunnable2 implements Runnable{
     public void run(){
         System.out.println("Thread " + Thread.currentThread().getName());
     }
@@ -6,8 +12,8 @@ class SimpleRunnable implements Runnable{
 
 public class Main{
     public static void main(String args[]) throws Exception{
-        Thread t1 = new Thread (new SimpleRunnable());
-        Thread t2 = new Thread (new SimpleRunnable());
+        Thread t1 = new Thread (new SimpleRunnable1());
+        Thread t2 = new Thread (new SimpleRunnable2());
         t1.setName("T1");
         t2.setName("T2");
         t1.start();
