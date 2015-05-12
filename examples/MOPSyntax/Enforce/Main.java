57394efee75cb18e1fb7b1cd9aa8bdaff79a926c
xiaohe27 <hexiao2@illinois.edu>
2015-05-11 23:40:33 -0500

    update enforcement test case

--- examples/MOPSyntax/Enforce/Main.java
+++ examples/MOPSyntax/Enforce/Main.java
@@ -8,10 +8,11 @@ public class Main{
     public static void main(String args[]) throws Exception{
         Thread t1 = new Thread (new SimpleRunnable());
         Thread t2 = new Thread (new SimpleRunnable());
+        t1.setName("T1");
+        t2.setName("T2");
         t1.start();
         t2.start();
         t1.join();
         t2.join();
     }
-
 }
