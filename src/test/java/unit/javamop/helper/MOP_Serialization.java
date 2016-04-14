b07bea9b2e26641317d69f912945f8e881e86afa
xiaohe27 <xiaoguoyi27@gmail.com>
2016-04-14 18:01:42 -0500

    code to generate trusted MOPSpecFile ast

--- src/test/java/unit/javamop/helper/MOP_Serialization.java
+++ src/test/java/unit/javamop/helper/MOP_Serialization.java
@@ -1,8 +1,13 @@
 package javamop.helper;
 
+import examples.ExamplesIT;
+import javamop.parser.SpecExtractor;
 import javamop.parser.ast.MOPSpecFile;
+import javamop.util.MOPException;
 
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Created by hx312 on 4/14/2016.
@@ -12,12 +17,12 @@ import java.io.*;
 public class MOP_Serialization {
     /**
      * Method for serializing an mop spec object to a file.
+     *
      * @param mopSpecFile The mop spec object that is going to be serialized.
-     * @param outputPath The path in which the serialization of mop spec object will be stored.
+     * @param outputPath  The path in which the serialization of mop spec object will be stored.
      */
     public static void writeMOPSpecObject2File(MOPSpecFile mopSpecFile, String outputPath) {
-        try
-        {
+        try {
             FileOutputStream fileOut =
                     new FileOutputStream(outputPath);
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
@@ -25,36 +30,61 @@ public class MOP_Serialization {
             out.close();
             fileOut.close();
             System.out.println("Serialized data of spec is stored into " + outputPath);
-        }catch(IOException i)
-        {
+        } catch (IOException i) {
             i.printStackTrace();
         }
     }
 
     /**
      * Deserialize an MOP spec object from a file.
+     *
      * @param inputPath The path to the serialization file.
      * @return The MOP spec object.
      */
     public static MOPSpecFile readMOPSpecObjectFromFile(String inputPath) {
         MOPSpecFile mopSpecFile = null;
 
-        try
-        {
+        try {
             FileInputStream fileIn = new FileInputStream(inputPath);
             ObjectInputStream in = new ObjectInputStream(fileIn);
             mopSpecFile = (MOPSpecFile) in.readObject();
             in.close();
             fileIn.close();
-        }catch(IOException i)
-        {
+        } catch (IOException i) {
             i.printStackTrace();
-        }catch(ClassNotFoundException c)
-        {
+        } catch (ClassNotFoundException c) {
             System.out.println("MOPSpecFile class not found.");
             c.printStackTrace();
         }
 
         return mopSpecFile;
+    }
+
+    /**
+     * Generate the trusted AST files, invoked once in version 4.4;
+     *
+     * @param args
+     * @throws MOPException
+     */
+    public static void main(String[] args) throws MOPException {
+        Path outputDir = Paths.get("src" + File.separator + "test" + File.separator
+                + "resources");
+        //generate some serialization files from mop specs.
+        Object[] inputList = ExamplesIT.data().toArray();
+
+        for (int i = 0; i < inputList.length; i++) {
+            if (i == 2 || i == 10 || i == 11 ||
+                    i == 15 || i == 18 || i == 19) {
+                Object[] inputI = (Object[]) inputList[i];
+                String path = (String) inputI[0];
+
+                File inputSpecFile = new File(path);
+                String testName = path.substring(path.lastIndexOf(File.separator) + 1);
+                String serFileOutputPath = outputDir.toAbsolutePath().toString()
+                        + File.separator + testName + ".ser";
+                MOPSpecFile specFile = SpecExtractor.parse(inputSpecFile);
+                writeMOPSpecObject2File(specFile, serFileOutputPath);
+            }
+        }
     }
 }
