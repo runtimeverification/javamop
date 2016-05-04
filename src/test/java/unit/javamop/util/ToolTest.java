5b53cfabb8c1af5062307f95258215defda09bbf
xiaohe27 <xiaoguoyi27@gmail.com>
2016-05-04 17:49:27 -0500

    update ToolTest

--- src/test/java/unit/javamop/util/ToolTest.java
+++ src/test/java/unit/javamop/util/ToolTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -21,68 +22,84 @@ public class ToolTest {
         }
     }
 
+    /**
+     * This test is in fact modelling the behavior of
+     * the original method so that the change of the implementation of
+     * the method "public static boolean Tool.isSpecFile(String)"
+     * will be noticed.
+     * @throws Exception
+     */
     @Test
     public void isSpecFile() throws Exception {
+        assertTrue(Tool.isSpecFile(".mop"));
+        assertTrue(Tool.isSpecFile("a.mop"));
+        assertTrue(Tool.isSpecFile("xyz.mop"));
 
+        assertFalse(Tool.isSpecFile("x.rvm"));
+
+        boolean throwEx = false;
+        try {
+            assertTrue(Tool.isSpecFile(null));
+        } catch (NullPointerException npe) {
+            throwEx = true;
+        }
+
+        assertTrue(throwEx);
     }
 
-    @Test
-    public void isListFile() throws Exception {
 
-    }
-
-    @Test
+//    @Test
     public void findBlockStart() throws Exception {
-
+        //TODO
     }
 
-    @Test
+//    @Test
     public void findBlockEnd() throws Exception {
-
+        //TODO
     }
 
-    @Test
+//    @Test
     public void removeComments() throws Exception {
-
+        //TODO
     }
 
-    @Test
+//    @Test
     public void changeIndentation() throws Exception {
-
+        //TODO
     }
 
-    @Test
+//    @Test
     public void getFileName() throws Exception {
-
+        //TODO
     }
 
-    @Test
+//    @Test
     public void convertFileToString() throws Exception {
-
+        //TODO
     }
 
-    @Test
+//    @Test
     public void convertFileToString1() throws Exception {
-
+        //TODO
     }
 
-    @Test
+//    @Test
     public void convertFileToStringSet() throws Exception {
-
+        //TODO
     }
 
-    @Test
+//    @Test
     public void polishPath() throws Exception {
-
+        //TODO
     }
 
-    @Test
+//    @Test
     public void getConfigPath() throws Exception {
-
+        //TODO
     }
 
-    @Test
+//    @Test
     public void deleteDirectory() throws Exception {
-
+        //TODO
     }
 }
