47c68b3510bb3c33291f394f8928fbef62415331
xiaohe27 <xiaoguoyi27@gmail.com>
2016-05-04 22:46:05 -0500

    test multi-property in single spec

--- src/test/java/system/examples/TestHelper.java
+++ src/test/java/system/examples/TestHelper.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertTrue;
 
 /**
  * Helper class for testing the output of external commands against the expected output.
+ *
  * @author TraianSF
  */
 public class TestHelper {
@@ -28,41 +29,44 @@ public class TestHelper {
     private final File basePathFile;
     private final Path basePath;
 
-    private final HashMap<String,String> envVars;
+    private final HashMap<String, String> envVars;
 
     /**
      * Initializes the {@code basePath} field to the parent directory of the specified file path
-     * @param filePath  path to the file which prompted this test, used to establish working dir
+     *
+     * @param filePath path to the file which prompted this test, used to establish working dir
      */
-    public TestHelper(String filePath)   {
+    public TestHelper(String filePath) {
         this(filePath, new HashMap<String, String>());
     }
 
-    public TestHelper(String filePath, HashMap<String,String> map) {
+    public TestHelper(String filePath, HashMap<String, String> map) {
         fileSystem = FileSystems.getDefault();
         this.basePath = fileSystem.getPath(filePath).getParent();
         basePathFile = basePath.toFile();
         this.envVars = map;
     }
-    
+
     /**
      * Execute command, tests return code and potentially checks standard and error output against expected content
      * in files if {@code expectedFilePrefix} not null.
+     *
      * @param expectedFilePrefix the prefix for the expected files, or null if output is not checked.
-     * @param command  list of arguments describing the system command to be executed.
+     * @param command            list of arguments describing the system command to be executed.
      * @throws Exception
      */
     public void testCommand(String expectedFilePrefix, String... command) throws Exception {
         testCommand(expectedFilePrefix, false, true, command);
     }
-    
+
     /**
      * Execute command, tests return code and potentially checks standard and error output against expected content
      * in files if {@code expectedFilePrefix} not null.
+     *
      * @param expectedFilePrefix the prefix for the expected files, or null if output is not checked.
-     * @param ignoreOrder when comparing contents of two files, whether to ignore the order of lines or not
-     * @param mustSucceed if the program's return code must be {@code 0}.
-     * @param commands  list of arguments describing the system command to be executed.
+     * @param ignoreOrder        when comparing contents of two files, whether to ignore the order of lines or not
+     * @param mustSucceed        if the program's return code must be {@code 0}.
+     * @param commands           list of arguments describing the system command to be executed.
      * @throws Exception
      */
     public void testCommand(String expectedFilePrefix, boolean ignoreOrder, boolean mustSucceed,
@@ -73,12 +77,13 @@ public class TestHelper {
     /**
      * Execute command, tests return code and potentially checks standard and error output against expected content
      * in files if {@code expectedFilePrefix} not null.
-     * @param relativePath The path relative to the MOP file directory to run commands from and find expected files in.
+     *
+     * @param relativePath       The path relative to the MOP file directory to run commands from and find expected files in.
      * @param expectedFilePrefix the prefix for the expected files, or null if output is not checked.
-     * @param ignoreOrder when comparing contents of two files, whether to ignore the order of lines or not
-     * @param mustSucceed if the program's return code must be {@code 0}.
-     * @param onlyCheckStdErr Only check whether there is error occurring during execution.
-     * @param command  list of arguments describing the system command to be executed.
+     * @param ignoreOrder        when comparing contents of two files, whether to ignore the order of lines or not
+     * @param mustSucceed        if the program's return code must be {@code 0}.
+     * @param onlyCheckStdErr    Only check whether there is error occurring during execution.
+     * @param command            list of arguments describing the system command to be executed.
      * @throws Exception
      */
     public void testCommand(String relativePath, String expectedFilePrefix, boolean ignoreOrder,
@@ -121,15 +126,14 @@ public class TestHelper {
 
         Process process = processBuilder.start();
         int returnCode = process.waitFor();
-        if(mustSucceed) {
+        if (mustSucceed) {
             Assert.assertEquals("Expected no error during" + Arrays.toString(command) + ".", 0, returnCode);
         }
         if (expectedFilePrefix != null && !onlyCheckStdErr) {
             if (!ignoreOrder) {
                 assertEqualFiles(expectedOutFile, actualOutFile);
                 assertEqualFiles(expectedErrFile, actualErrFile);
-            }
-            else {
+            } else {
                 assertEqualUnorderedFiles(expectedOutFile, actualOutFile);
                 assertEqualUnorderedFiles(expectedErrFile, actualErrFile);
             }
@@ -150,29 +154,33 @@ public class TestHelper {
         int returnCode = process.waitFor();
 
         assertTrue("Output does not exist!", outputUnderTest != null &&
-                                            outputUnderTest.toFile().exists());
+                outputUnderTest.toFile().exists());
 
         assertTrue("Given property does not hold on output " + outputUnderTest.getFileName().toString(),
                 property.test(new String(Files.readAllBytes(outputUnderTest))));
     }
+
     /**
      * Assert two files have equal content.
+     *
      * @param expectedFile The path to the file with the expected result.
-     * @param actualFile The path to the file with the calculated result.
+     * @param actualFile   The path to the file with the calculated result.
      */
     public void assertEqualFiles(String expectedFile, String actualFile) throws IOException {
         String expectedText = Tool.convertFileToString(expectedFile);
         String actualText = Tool.convertFileToString(actualFile);
-        
+
         Assert.assertEquals(actualFile + " should match " + expectedFile, expectedText, actualText);
     }
 
     /**
      * Assert two files have the same contents, but lines can have different order.
+     *
      * @param expectedFile The path to the file with the expected result.
-     * @param actualFile The path to the file with the calculated result.
+     * @param actualFile   The path to the file with the calculated result.
      */
-    public void assertEqualUnorderedFiles(String expectedFile, String actualFile) throws IOException {
+    public static void assertEqualUnorderedFiles(String expectedFile, String actualFile) throws
+            IOException {
         Set<String> expectedText = Tool.convertFileToStringSet(expectedFile);
         Set<String> actualText = Tool.convertFileToStringSet(actualFile);
 
@@ -181,8 +189,9 @@ public class TestHelper {
 
     /**
      * Assert two files have the same contents, ignoring the differences in line separators.
+     *
      * @param expectedFile The path to the file with the expected result.
-     * @param actualFile The path to the file with the calculated result.
+     * @param actualFile   The path to the file with the calculated result.
      * @throws IOException
      */
     public void assertEqualFilesIgnoringLineSeparators(String expectedFile, String actualFile) throws IOException {
@@ -194,7 +203,8 @@ public class TestHelper {
 
     /**
      * Moves files from the current directory to the path pointed to by basePath
-     * @param files  files to be relocated
+     *
+     * @param files files to be relocated
      * @throws IOException
      */
 
@@ -210,6 +220,7 @@ public class TestHelper {
 
     /**
      * Deletes files from the basePath, potentially failing if the files don't exist
+     *
      * @param fail  if true, it expects the files to exist and fails the test if they don't
      * @param files relative paths (to basePath) of the files to be deleted
      * @throws IOException
@@ -236,7 +247,8 @@ public class TestHelper {
     /**
      * Computes the path obtained by adding the relative path specified by {@code path} to the
      * {@code basePath}
-     * @param path  path relative to {@code basePath} to be computed
+     *
+     * @param path path relative to {@code basePath} to be computed
      * @return the path obtained by adding {@code path} to {@code basePath}
      */
     public Path getPath(String path) {
--- src/test/java/unit/javamop/input/JavaMOPMainTest.java
+++ src/test/java/unit/javamop/input/JavaMOPMainTest.java
@@ -1,9 +1,13 @@
 package javamop.input;
 
 import javamop.JavaMOPMain;
+import javamop.helper.IOUtils;
+import org.eclipse.core.internal.dtree.TestHelper;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
@@ -32,6 +36,26 @@ public class JavaMOPMainTest {
     public void testInputFileWithNonMOPExtension() {
         String[] args = new String[]{"./src/test/java/unit/javamop/input/HasNext.mo"};
         JavaMOPMain.main(args); //expect normal exit.
+    }
+
+    //TODO: multiple specs
+    //TODO: With Invalid option
+    //TODO: valid option
+
+    @Test
+    public void testMultiPropertyInSingleSpec() throws IOException {
+        String[] args = new String[] {"./src/test/resources/multi-property/UnsafeIterator.mop"};
+        JavaMOPMain.main(args);
+
+        examples.TestHelper.assertEqualUnorderedFiles(
+                "./src/test/resources/multi-property/UnsafeIterator.rvm",
+                "./target/test-classes/multi-property/UnsafeIterator.rvm");
+
+        examples.TestHelper.assertEqualUnorderedFiles(
+                "./src/test/resources/multi-property/UnsafeIteratorMonitorAspect.aj",
+                "./target/test-classes/multi-property/UnsafeIteratorMonitorAspect.aj"
+        );
+
     }
 
 }
