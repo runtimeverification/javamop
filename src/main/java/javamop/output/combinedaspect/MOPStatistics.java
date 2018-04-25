6b602baccf1c3a97d7e8123b0a132f9ea34ffe31
Owolabi Legunsen <legunse2@illinois.edu>
2018-04-25 05:01:14 +0000

    change to record statistics

--- src/main/java/javamop/output/combinedaspect/MOPStatistics.java
+++ src/main/java/javamop/output/combinedaspect/MOPStatistics.java
@@ -22,11 +22,11 @@ public class MOPStatistics {
     private final String categoryCountersMethodName = "getCategoryCounters";
 
     private final HashMap<String, MOPVariable> eventVars = new HashMap<String, MOPVariable>();
-    private final HashMap<PropertyAndHandlers, HashMap<String, MOPVariable>> categoryVars = 
+    private final HashMap<PropertyAndHandlers, HashMap<String, MOPVariable>> categoryVars =
         new HashMap<PropertyAndHandlers, HashMap<String, MOPVariable>>();
-    private final HashMap<MOPParameter, MOPVariable> paramVars = 
+    private final HashMap<MOPParameter, MOPVariable> paramVars =
         new HashMap<MOPParameter, MOPVariable>();
-    
+
     private final String specName;
     private final boolean isRaw;
 
@@ -40,28 +40,28 @@ public class MOPStatistics {
         this.isRaw = mopSpec.isRaw();
 
         for (EventDefinition event : mopSpec.getEvents()) {
-            MOPVariable eventVar = new MOPVariable(mopSpec.getName() + "_" + event.getId() + 
+            MOPVariable eventVar = new MOPVariable(mopSpec.getName() + "_" + event.getId() +
                 "_num");
             this.eventVars.put(event.getId(), eventVar);
         }
-        
+
         for (PropertyAndHandlers prop : mopSpec.getPropertiesAndHandlers()) {
             HashMap<String, MOPVariable> categoryVarsforProp = new HashMap<String, MOPVariable>();
             for (String key : prop.getHandlers().keySet()) {
-                MOPVariable categoryVar = new MOPVariable(mopSpec.getName() + "_" + 
+                MOPVariable categoryVar = new MOPVariable(mopSpec.getName() + "_" +
                     prop.getPropertyId() + "_" + key + "_num");
                 categoryVarsforProp.put(key, categoryVar);
             }
             this.categoryVars.put(prop, categoryVarsforProp);
         }
-        
+
         for (MOPParameter param : mopSpec.getParameters()) {
-            MOPVariable paramVar = new MOPVariable(mopSpec.getName() + "_" + param.getName() + 
+            MOPVariable paramVar = new MOPVariable(mopSpec.getName() + "_" + param.getName() +
                 "_set");
             this.paramVars.put(param, paramVar);
         }
     }
-    
+
     /**
      * AspectJ advice code to display the statistics after termination of the program.
      * @return AspectJ/Java source code displaying statistics.
@@ -71,23 +71,25 @@ public class MOPStatistics {
         if (!JavaMOPMain.options.statistics)
             return ret;
 
-        String monitorName = specName + (isRaw ? "Raw" : "") + "Monitor";
+        // String monitorName = specName + (isRaw ? "Raw" : "") + "Monitor";
+        String monitorName = specName + "Monitor";
 
-        ret += "after () : execution(* *.main(..)) {\n";
-        
-        ret += "System.err.println(\"== " + this.specName + " ==\");\n";
+        // ret += "after () : execution(* *.main(..)) {\n";
+        ret += "after () : execution(* org.apache.maven.surefire.booter.ForkedBooter.runSuitesInProcess(..)) {\n";
+
+        ret += "System.err.println(\"==start " + this.specName + " ==\");\n";
 
         ret += "System.err.println(\"#monitors: \" + " + monitorName + "." + this.totalMonitorMethodName + "());\n";
         ret += "System.err.println(\"#collected monitors: \" + " + monitorName + "."
                 + this.collectedMonitorMethodName + "());\n";
         ret += "System.err.println(\"#terminated monitors: \" + " + monitorName + "."
                 + this.terminatedMonitorMethodName + "());\n";
-        
+
         for (String eventName : eventVars.keySet()) {
             ret += "System.err.println(\"#event - " + eventName + ": \" + " + monitorName + "."
                     + this.eventCountersMethodName + "()" + ".get(\""  + eventName + "\")" +");\n";
         }
-        
+
         for (PropertyAndHandlers prop : categoryVars.keySet()) {
             HashMap<String, MOPVariable> categoryVarsforProp = categoryVars.get(prop);
             for (String categoryName : categoryVarsforProp.keySet()) {
@@ -96,15 +98,16 @@ public class MOPStatistics {
                         + this.categoryCountersMethodName + "()" + ".get(\""  + categoryName + "\")" +");\n";
             }
         }
-        
+
+        ret += "System.err.println(\"==end " + this.specName + " ==\");\n";
         // for(MOPParameter param : paramVars.keySet()){
         // MOPVariable paramVar = paramVars.get(param);
         // ret += "System.err.println(\"#parameter - " + param.getName() +
         // ": \" + " + paramVar + ".size()" + ");\n";
         // }
-        
+
         ret += "}\n";
-        
+
         return ret;
     }
 
