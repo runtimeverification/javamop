ef20b0769aeec6054768c3e5f3d2606e3c7f94e5
Owolabi Legunsen <legunsen@cornell.edu>
2022-01-31 10:07:36 -0500

    (WIP) nits

--- src/test/java/unit/javamop/parser/SpecExtractorTest.java
+++ src/test/java/unit/javamop/parser/SpecExtractorTest.java
@@ -2,11 +2,7 @@ package javamop.parser;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
