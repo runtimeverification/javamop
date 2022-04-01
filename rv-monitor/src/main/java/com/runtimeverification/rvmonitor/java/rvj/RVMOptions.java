22461d8228534bda76be18ddf28c3daf58c3367f
Owolabi Legunsen <legunsen@cornell.edu>
2022-04-01 09:43:09 -0400

    describe what some options do

--- rv-monitor/src/main/java/com/runtimeverification/rvmonitor/java/rvj/RVMOptions.java
+++ rv-monitor/src/main/java/com/runtimeverification/rvmonitor/java/rvj/RVMOptions.java
@@ -36,14 +36,20 @@ public class RVMOptions {
     @Parameter(names={"-n","-name"},description = "Use the given name instead of source file name.")
     public String name;
 
-    @Parameter(names={"-merge"},description = "TBD")
-    public boolean merge;
+    @Parameter(names="-noadvicebody",description = "Enabling this causes RV-Monitor not to put " +
+            "the monitoring-related routine inside the corresponding advice body. This can be " +
+            "used count the number of times that each event fired.")
+    public boolean noadvicebody = false;
 
-    @Parameter(names={"-inline"},description = "TBD")
-    public boolean inline;
+    @Parameter(names="-merge",description = "This option enables RV-Monitor to use the name of the combined .aj file" +
+            " that JavaMOP produced")
+    public boolean merge = false;
 
-    @Parameter(names={"-noadvicebody"},description = "TBD")
-    public boolean noadvicebody;
+    @Parameter(names="-inline",description = "If -inline is disabled, the monitoring-related " +
+            "routine (which is printed in adviceBody()) is put inside of the corresponding " +
+            "advice. If it's enabled, the routine is promoted to a Java method and this method " +
+            "is invoked by the corresponding advice.")
+    public boolean inline = false;
 
     @Parameter(names={"-finegrainedlock"},description = "Use fine-grained lock for internal data structure.")
     public boolean finegrainedlock;
@@ -51,13 +57,13 @@ public class RVMOptions {
     @Parameter(names={"-weakrefinterning"},description = "Use WeakReference interning in indexing trees.")
     public boolean weakrefinterning;
 
-    @Parameter(names={"-partitionedset"},description = "TBD")
+    @Parameter(names={"-partitionedset"},description = "Experimental(?)")
     public boolean partitionedset;
 
-    @Parameter(names={"-atomicmonitor"},description = "TBD")
+    @Parameter(names={"-atomicmonitor"},description = "Experimental(?)")
     public boolean atomicmonitor;
 
-    @Parameter(names={"-version"},description = "TBD")
+    @Parameter(names={"-version"},description = "Display RV-Monitor version information.")
     public boolean version;
 
     public boolean isJarFile;
