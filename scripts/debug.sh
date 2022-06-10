3b254c2ff649fb099bb8d70abeaecf159f2950ab
Owolabi Legunsen <legunsen@cornell.edu>
2022-06-10 06:40:25 -0400

    refactor debug script

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,9 +1,13 @@
-mvn clean package -DskipTests -DskipITs && rv-monitor -d out -debug ${HOME}/projects/javamop/rv-monitor/examples/java/FSM/HasNext2/rvm -v
+if [[ $1 == "build" ]]; then
+    mvn clean package -DskipTests -DskipITs
+fi
 
-# mvn clean package -DskipTests -DskipITs && rv-monitor -d out -debug ${HOME}/projects/javamop/rv-monitor/examples/java/ERE/UnsafeMapIterator/rvm -v
+rv-monitor -d out -debug ${HOME}/projects/javamop/rv-monitor/examples/java/FSM/HasNext2/rvm -v
 
-# mvn clean package -DskipTests -DskipITs && rv-monitor -d out -debug ${HOME}/projects/javamop/rv-monitor/examples/java/CFG/SafeFileWriter/rvm -v
+# rv-monitor -d out -debug ${HOME}/projects/javamop/rv-monitor/examples/java/ERE/UnsafeMapIterator/rvm -v
 
-# mvn clean package -DskipTests -DskipITs && rv-monitor -d out -debug ${HOME}/projects/javamop/rv-monitor/examples/java/SRS/EqualityCheck/rvm -v
+# rv-monitor -d out -debug ${HOME}/projects/javamop/rv-monitor/examples/java/CFG/SafeFileWriter/rvm -v
 
-# mvn clean package -DskipTests -DskipITs && rv-monitor -d out -debug ${HOME}/projects/javamop/rv-monitor/examples/java/LTL/SafeMapIterator/rvm -v
+# rv-monitor -d out -debug ${HOME}/projects/javamop/rv-monitor/examples/java/SRS/EqualityCheck/rvm -v
+
+# rv-monitor -d out -debug ${HOME}/projects/javamop/rv-monitor/examples/java/LTL/SafeMapIterator/rvm -v
