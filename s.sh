cc58448c30929426cf930261b5d2fa575f873d45
Owolabi Legunsen <legunsen@cornell.edu>
2022-06-29 16:12:35 -0400

    update the test script to match the fact that we now have observers

--- s.sh
+++ s.sh
@@ -1,13 +1,11 @@
-javac props/classes/mop/*.java
-
-javamopagent -emop props/ props/classes -n JavaMOPAgent -v
-
-mv JavaMOPAgent.jar props/
-
-mvn install:install-file -Dfile=props/JavaMOPAgent.jar -DgroupId="javamop-agent" -DartifactId="javamop-agent" -Dversion="1.0" -Dpackaging="jar"
+mvn install:install-file -Dfile=agents/JavaMOPAgent.jar -DgroupId="javamop-agent" -DartifactId="javamop-agent" -Dversion="1.0" -Dpackaging="jar"
 
 cd /tmp/commons-fileupload
 
 rm -rf violation-counts
 
+rm -rf /tmp/internal.txt
+
 mvn test -Dtest=org.apache.commons.fileupload2.FileItemHeadersTest
+
+tail /tmp/internal.txt
