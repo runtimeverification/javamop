cf1a51acf1f53f34e0f971f11529c0b901722af9
Owolabi Legunsen <legunsen@cornell.edu>
2022-12-19 14:12:33 -0500

    try new make-agent script

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -46,7 +46,7 @@ check_status "${mop_test_status}" " on mop's unit tests"
     cd ${dir}
     git checkout -f h2
     git clean -ffxd
-    bash make-agent-new.sh props/ ${SCRIPT_DIR}/agents quiet
+    bash make-agent.sh props/ ${SCRIPT_DIR}/agents quiet
     mvn install:install-file -Dfile=${SCRIPT_DIR}/agents/JavaMOPAgent.jar -DgroupId="javamop-agent" -DartifactId="javamop-agent" -Dversion="1.0" -Dpackaging="jar"
 ) &> /tmp/agent-outcome.txt
 
