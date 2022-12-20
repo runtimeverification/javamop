0caaca406a58c5855f8d02d0e4663a1f57164256
Pengyue Jiang <pengyue3@illinois.edu>
2022-12-20 10:33:05 -0500

    Change sed to work both for GNU and BSD

--- scripts/install-javaparser.sh
+++ scripts/install-javaparser.sh
@@ -4,7 +4,8 @@
     (
         cd javaparser
         git checkout javaparser-parent-3.23.1
-        sed -i 's/public final int hashCode/public int hashCode/' javaparser-core/src/main/java/com/github/javaparser/ast/Node.java
+        sed -i.bak 's/public final int hashCode/public int hashCode/' javaparser-core/src/main/java/com/github/javaparser/ast/Node.java
+        rm javaparser-core/src/main/java/com/github/javaparser/ast/Node.java.bak
         mvn install -DskipTests -DskipITs
     )
 )
