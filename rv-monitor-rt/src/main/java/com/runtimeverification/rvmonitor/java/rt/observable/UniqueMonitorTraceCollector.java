9b2c8b8c7845e9465be9eebee4f66bcd5987f748
Owolabi Legunsen <legunsen@cornell.edu>
2022-12-26 17:35:06 -0500

    yet another hack

--- rv-monitor-rt/src/main/java/com/runtimeverification/rvmonitor/java/rt/observable/UniqueMonitorTraceCollector.java
+++ rv-monitor-rt/src/main/java/com/runtimeverification/rvmonitor/java/rt/observable/UniqueMonitorTraceCollector.java
@@ -26,9 +26,9 @@ public class UniqueMonitorTraceCollector extends AllMonitorTraceCollector {
         if (isDoAnalysis()) {
             analyzeUniqueTraces();
         }
-        if (isDumpingTraces()) {
+//        if (isDumpingTraces()) {
             traceDB.dump();
-        }
+//        }
     }
 
     private String getFrequencyMap(Map<String, Integer> traceFrequencyMap) {
